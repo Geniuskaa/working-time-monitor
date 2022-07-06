c7e611de578d069bf6177e3cc4f5d27bab60a935
ibragimovaidar <ibragimovaidarp@gmail.com>
2022-07-06 14:17:53 +0300

    add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type App struct {
+}
+
+type DB struct {
+}
+
+type Config struct {
+	App App `yaml:"app"`
+	DB  DB  `yaml:"db"`
+}
+
+func NewConfig(profile string) (*Config, error) {
+	cfg := &Config{}
+	cfgPath := fmt.Sprintf("configs/%s.yml", profile)
+	err := cleanenv.ReadConfig(cfgPath, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
