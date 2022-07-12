43f61c10cc41c07dcb263f261b7ea6a386b0274e
ibragimovaidar <ibragimovaidarp@gmail.com>
2022-07-12 10:56:35 +0300

    fix repo methods

--- internal/postgres/deviceRepo.go
+++ internal/postgres/deviceRepo.go
@@ -57,11 +57,10 @@ func (d *Db) GetMobileDevicesByOs(ctx context.Context, os string) ([]*model.Mobi
 }
 
 func (d *Db) GetLatestRentingDeviceByDeviceId(ctx context.Context, deviceId int) (*model.RentingDevice, error) {
-	q := "SELECT d.id, d.created_at, d.updated_at, u.id, u.username, u.display_name " +
-		"FROM renting_devices d " +
-		"INNER JOIN users u ON d.user_id = u.id " +
-		"WHERE d.mobile_device_id = $1 " +
-		"ORDER BY d.created_at DESC LIMIT 1"
+	q := `SELECT d.id, d.created_at, d.updated_at, u.id, u.username, u.display_name 
+FROM renting_devices d INNER JOIN users u ON d.user_id = u.id 
+WHERE d.mobile_device_id = $1 
+ORDER BY d.created_at DESC LIMIT 1`
 	row := d.Db.QueryRowContext(ctx, q, deviceId)
 	device := model.RentingDevice{MobileDevice: model.MobileDevice{Id: deviceId}}
 	err := row.Scan(&device.Id, &device.CreatedAt, &device.UpdatedAt, &device.User.Id, &device.User.Username, &device.User.DisplayName)
@@ -72,9 +71,9 @@ func (d *Db) GetLatestRentingDeviceByDeviceId(ctx context.Context, deviceId int)
 }
 
 func (d *Db) GetRentingDeviceById(ctx context.Context, id int) (*model.RentingDevice, error) {
-	q := "SELECT d.id, d.mobile_device_id, d.created_at, d.updated_at, u.id, u.username, u.display_name " +
-		"FROM renting_devices d " +
-		"INNER JOIN users u on d.user_id = u.id WHERE d.id = $1"
+	q := `SELECT d.id, d.mobile_device_id, d.created_at, d.updated_at, u.id, u.username, u.display_name 
+FROM renting_devices d INNER JOIN users u on d.user_id = u.id 
+WHERE d.id = $1`
 	row := d.Db.QueryRowContext(ctx, q, id)
 	device := model.RentingDevice{}
 	err := row.Scan(&device.Id, &device.MobileDevice.Id, &device.CreatedAt, &device.UpdatedAt, &device.User.Id,
@@ -86,7 +85,7 @@ func (d *Db) GetRentingDeviceById(ctx context.Context, id int) (*model.RentingDe
 }
 
 func (d *Db) SaveRentingDevice(ctx context.Context, rentingDevice *model.RentingDevice) (int, error) {
-	q := "INSERT INTO renting_devices(mobile_device_id, user_id, created_at) VALUES ($1, $2, $3) RETURNING id"
+	q := `INSERT INTO renting_devices(mobile_device_id, user_id, created_at) VALUES ($1, $2, $3) RETURNING id`
 	id := 0
 	err := d.Db.QueryRowContext(ctx, q, rentingDevice.MobileDevice.Id, rentingDevice.User.Id, rentingDevice.CreatedAt).Scan(&id)
 	if err != nil {
@@ -96,7 +95,7 @@ func (d *Db) SaveRentingDevice(ctx context.Context, rentingDevice *model.Renting
 }
 
 func (d *Db) UpdateRentingDevice(ctx context.Context, id int, device *model.RentingDevice) error {
-	q := "UPDATE renting_devices SET mobile_device_id = $2, user_id = $3, updated_at = $4 WHERE id = $1"
+	q := `UPDATE renting_devices SET mobile_device_id = $2, user_id = $3, updated_at = $4 WHERE id = $1`
 	result, err := d.Db.Exec(q, id, device.MobileDevice.Id, device.User.Id, device.UpdatedAt)
 	if err != nil {
 		return err
@@ -109,7 +108,7 @@ func (d *Db) UpdateRentingDevice(ctx context.Context, id int, device *model.Rent
 }
 
 func (d *Db) GetMobileDeviceById(ctx context.Context, id int) (*model.MobileDevice, error) {
-	q := "SELECT id, name, os FROM mobile_devices WHERE id = $1"
+	q := `SELECT id, name, os FROM mobile_devices WHERE id = $1`
 	row := d.Db.QueryRowContext(ctx, q, id)
 	device := model.MobileDevice{}
 	err := row.Scan(&device.Id, &device.Name, &device.Os)
