5be74d869f8fc675c27b0a41934cee6ce62f8892
ibragimovaidar <ibragimovaidarp@gmail.com>
2022-07-14 14:14:10 +0300

    add device error handling

--- internal/device/handler.go
+++ internal/device/handler.go
@@ -29,8 +29,8 @@ func NewHandler(ctx context.Context, log *zap.SugaredLogger, service Service) Ha
 func (h *handler) Routes() chi.Router {
 	r := chi.NewRouter()
 	r.Get("/", apperror.NewWrapper(h.log, h.getMobileDevices).Handle)
-	r.Get("/rent/{id}", h.rentDevice)
-	r.Get("/return/{id}", h.returnDevice)
+	r.Get("/rent/{id}", apperror.NewWrapper(h.log, h.rentDevice).Handle)
+	r.Get("/return/{id}", apperror.NewWrapper(h.log, h.returnDevice).Handle)
 	return r
 }
 
@@ -55,78 +55,54 @@ func (h *handler) getMobileDevices(w http.ResponseWriter, r *http.Request) error
 	return nil
 }
 
-func (h *handler) rentDevice(w http.ResponseWriter, r *http.Request) {
+func (h *handler) rentDevice(w http.ResponseWriter, r *http.Request) error {
 	tr := otel.Tracer("RentDevice")
 	ctx, span := tr.Start(h.ctx, "handler-RentDevice")
 	defer span.End()
 
 	principal, err := auth.GetUserPrincipal(r, ctx)
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return apperror.ErrUnauthorized
 	}
 	deviceId, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return err
 	}
 	d, err := h.service.RentDevice(ctx, deviceId, principal.Id)
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 	body, err := json.Marshal(d)
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 	w.Header().Add("Content-Type", "application/json")
-	_, err = w.Write(body)
-	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	w.Write(body)
+	return nil
 }
 
-func (h *handler) returnDevice(w http.ResponseWriter, r *http.Request) {
+func (h *handler) returnDevice(w http.ResponseWriter, r *http.Request) error {
 	tr := otel.Tracer("ReturnDevice")
 	ctx, span := tr.Start(h.ctx, "handler-ReturnDevice")
 	defer span.End()
 
 	principal, err := auth.GetUserPrincipal(r, ctx)
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return apperror.ErrUnauthorized
 	}
 	deviceId, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return apperror.NewBadRequestError(err, "Device id is not provided")
 	}
 	d, err := h.service.ReturnDevice(ctx, deviceId, principal.Id)
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return err
 	}
 	body, err := json.Marshal(d)
 	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 	w.Header().Add("Content-Type", "application/json")
-	_, err = w.Write(body)
-	if err != nil {
-		h.log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	w.Write(body)
+	return nil
 }
